7a2e132896bcb9d18625fd864c1819df978b225f
Mohsen Zainalpour <zainalpour@yahoo.com>
2016-05-01 15:18:17 +0430

    Add solutions for p16-p20

---
+++ src/main/scala/com/github/etacassiopeia/s99/P20.scala
@@ -0,0 +1,28 @@
+package com.github.etacassiopeia.s99
+
+/**
+  * <h1>P20</h1>
+  * Remove Kth element fom a list
+  *
+  * @author Mohsen Zainalpour
+  * @version 1.0
+  * @since 1/05/16 
+  */
+object P20 {
+  def main(args: Array[String]) {
+    println(removeAt(1, List('a, 'b, 'c, 'd)))
+  }
+
+  def removeAt[T](i: Int, list: List[T]): (List[T], T) = {
+
+    def inline(in: Int, l: List[T], mem: List[T]): (List[T], T) = {
+      l match {
+        case head :: tail if in > 0 => inline(in - 1, tail, head +: mem)
+        case head :: tail => (mem ::: tail, head)
+        case Nil => throw new NoSuchElementException
+      }
+    }
+
+    inline(i, list, List[T]())
+  }
+}
