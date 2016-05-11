691fb087314ddfe7c8789e90520c600e37440278
Mohsen Zainalpour <zainalpour@yahoo.com>
2016-05-11 16:12:14 +0430

    A response to a question was asked by my colleague

--- src/main/scala/com/github/etacassiopeia/s99/P06.scala
+++ src/main/scala/com/github/etacassiopeia/s99/P06.scala
@@ -11,10 +11,17 @@ package com.github.etacassiopeia.s99
 object P06 {
   def main(args: Array[String]) {
     println(isPalindrome(List(1, 2, 3, 2, 1)))
+
+    println(+:.unapply(List(1, 2, 3, 2, 1)) match {
+      //res0: Option[(Int,List[Int])] = Some(1,(List(1, 2, 3, 2)))
+      case Some((x, l)) => :+.unapply(l) //res1: Some((List(2, 3, 2),1))
+      case _ =>
+    })
   }
 
   def isPalindrome[T](list: List[T]): Boolean = list match {
-    case x +: l :+ y => x==y && isPalindrome(l)   //two different operator mention that <l> is a list
+    //case x +: l :+ y => {println(l); x == y && isPalindrome(l);} //two different operator mention that <l> is a list
+    case +:(x, :+(l, y)) => x == y && isPalindrome(l) //infix notation
     case _ => true
   }
 }
