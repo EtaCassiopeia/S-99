3fb1b877182384415962088e8e60bb2e8040bd9e
Mohsen Zainalpour <zainalpour@yahoo.com>
2016-05-14 12:40:02 +0430

    Check boundary,Issue #1

--- src/main/scala/com/github/etacassiopeia/s99/P21.scala
+++ src/main/scala/com/github/etacassiopeia/s99/P21.scala
@@ -14,9 +14,13 @@ object P21 {
     println(insertAt('new, 1, List('a, 'b, 'c, 'd)))
     println(insertAt('new, 0, List('a, 'b, 'c, 'd)))
     println(insertAt('new, 3, List('a, 'b, 'c, 'd)))
+    println(insertAt('new, 4, List('a, 'b, 'c, 'd)))
   }
 
   def insertAt[T](e: T, position: Int, list: List[T]): List[T] = {
+    if (position >= list.length)
+      throw new NoSuchElementException
+
     list match {
       case head :: tail if position == 0 => (e :: head :: Nil) ::: tail
       case head :: tail => head :: insertAt(e, position - 1, tail)
