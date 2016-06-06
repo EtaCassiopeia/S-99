f8f2261c2fe3ff80771aeabc8527db81277c9ad4
Mohsen Zainalpour <zainalpour@yahoo.com>
2016-06-06 12:22:21 +0430

    Add solution for P56

--- src/main/scala/com/github/etacassiopeia/s99/btree/P56.scala
+++ src/main/scala/com/github/etacassiopeia/s99/btree/P56.scala
@@ -2,12 +2,21 @@ package com.github.etacassiopeia.s99.btree
 
 /**
   * <h1>P56</h1>
-  * The P56 
+  * Symmetric binary trees.
+  * Let us call a binary tree symmetric if you can draw a vertical line through the root node and then the right subtree
+  * is the mirror image of the left subtree.
+  * Add an isSymmetric method to the Tree class to check whether a given binary tree is symmetric.
+  * Hint: Write an isMirror method first to check whether one tree is the mirror image of another.
+  * We are only interested in the structure, not in the contents of the nodes.
   *
   * @author Mohsen Zainalpour
   * @version 1.0
   * @since 6/06/16 
   */
 object P56 {
-
+  def main(args: Array[String]) {
+    println(Node('a', Node('b'), Node('c')).isSymmetric)
+    println(Node('a', Node('b'), Node('c', End, Node('d))).isSymmetric)
+    print(Node('a').isSymmetric)
+  }
 }
--- src/main/scala/com/github/etacassiopeia/s99/btree/Tree.scala
+++ src/main/scala/com/github/etacassiopeia/s99/btree/Tree.scala
@@ -14,6 +14,10 @@ abstract sealed class Tree[+T] {
   def height: Int
 
   def isBalanced: Boolean
+
+  def isSymmetric: Boolean
+
+  def isMirrorOf[T](other: Tree[T]): Boolean
 }
 
 case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
@@ -22,6 +26,13 @@ case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
   override def height: Int = Math.max(left.height + 1, right.height + 1)
 
   override def isBalanced: Boolean = Math.abs(left.height - right.height) <= 1
+
+  override def isSymmetric: Boolean = left.isMirrorOf(right)
+
+  override def isMirrorOf[T](other: Tree[T]): Boolean = (this, other) match {
+    case (Node(v1, lNode1, rNode1), Node(v2, lNode2, rNode2)) => lNode1.isMirrorOf(rNode2) && rNode1.isMirrorOf(lNode2)
+    case _ => false
+  }
 }
 
 case object End extends Tree[Nothing] {
@@ -30,6 +41,13 @@ case object End extends Tree[Nothing] {
   override def height: Int = 0
 
   override def isBalanced: Boolean = true
+
+  override def isSymmetric: Boolean = true
+
+  override def isMirrorOf[T](other: Tree[T]): Boolean = other match {
+    case End => true
+    case _ => false
+  }
 }
 
 object Node {
